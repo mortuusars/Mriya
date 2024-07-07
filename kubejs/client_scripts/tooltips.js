d645a2946f8565ec647228a8c06802f418ee6a7c
mortuusars <mortuusars@gmail.com>
2024-07-07 04:00:19 +0300

    Small fixes

--- kubejs/client_scripts/tooltips.js
+++ kubejs/client_scripts/tooltips.js
@@ -4,16 +4,20 @@ ItemEvents.tooltip(event => {
     }
 
     event.addAdvanced(['minecraft:wooden_hoe', 'minecraft:stone_hoe', 'minecraft:iron_hoe', 
-        'minecraft:golden_hoe', 'minecraft:diamond_hoe', 'minecraft:netherite_hoe', 
-        'additionaladditions:gilded_netherite_hoe'], (item, advanced, text) => {
-            text.add(1, Component.gray('Can harvest and replant wide area of crops'));
-        })
+        'minecraft:golden_hoe', 'minecraft:diamond_hoe', 'minecraft:netherite_hoe'], 
+        (item, advanced, text) => {
+            text.add(1, Component.gray('Use on crops to harvest and replant a wide area'));
+    })
 
-    
+    event.addAdvanced(['minecraft:wooden_axe', 'minecraft:stone_axe', 'minecraft:iron_axe', 
+        'minecraft:golden_axe', 'minecraft:diamond_axe', 'minecraft:netherite_axe'], 
+        (item, advanced, text) => {
+            text.add(1, Component.gray('Hold ').append(Component.white('[Sneak]')).append(Component.gray(' to fell a tree')));
+    })
 
     // Misc:
     event.addAdvanced('ecologics:seashell', (item, advanced, text) => {
-        text.add(1, Component.gray('[Use]').append(Component.darkGray(' to break open.')));
+        text.add(1, Component.white('[Use]').append(Component.gray(' to break open')));
     });
 
     event.addAdvanced('spelunkery:salt', (item, advanced, text) => {
@@ -33,8 +37,8 @@ ItemEvents.tooltip(event => {
     event.addAdvanced('minecraft:redstone', (item, advanced, text) => {
         text.remove(0);
     });
-
     event.addAdvanced(/comforts:sleeping_bag_/, (item, advanced, text) => {
         text.remove(1);
     })
+
 })
